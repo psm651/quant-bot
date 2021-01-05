c3939d93aa04a36b653e3c7a50e1027f6d5e3cd7
unknown <psm651@naver.com>
2021-01-05 23:37:31 +0900

    봇자동화 인강

---
+++ my_package/sum.py
@@ -0,0 +1,11 @@
+def sum_ab(x, y):
+    return x + y
+
+
+class Calculator:
+    def __init__(self, cx, cy):
+        self.x = cx
+        self.y = cy
+
+    def sum(self):
+        return self.x + self.y
